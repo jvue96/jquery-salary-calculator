8594445d3640bc2757bac4d2901136d83098d38a
Juno Vue <Juno.Vue@Hotmail.com>
2019-03-17 22:07:37 -0500

    corrected format, moved delete button call to function OnReady. Monthly costs background color changes to red when exceed $20,000

--- client.js
+++ client.js
@@ -1,93 +1,83 @@
 $(document).ready(readyNow);
 
-// global variables 
+// global variables
 let newEmployeeArray = [];
-let monthsInYear = 12; 
+let monthsInYear = 12;
 let monthlyCostCap = 20000;
 
-function readyNow () {
-    appendEmployees();
-    deleteButton();
+function readyNow() {
+  appendEmployees();
+  $("#employeeTable").on("click", ".deleteButton", deleteButton);
 } // end readyNow
 
-
-function deleteButton () {
-    $('#employeeTable').on('click', '.deleteButton', deleteButton1)
-function deleteButton1 () {
-$(this).closest('tr').remove();
-    }
-} // end deleteButton 
+// append employee properties on submit button click
+function appendEmployees() {
+  $("#submitButton").on("click", appendAllInputs);
+} // end appendEmployees
 
 // on click of submit button, appendAllInputs function will add a new employee with these inputs
-function appendAllInputs () {
-    let newEmployee = {
-         first: firstName = $('#firstName').val(),
-         last: lastName = $('#lastName').val(),
-         id: employeeId = $('#idNumber').val(),
-         title: title = $('#employeeTitle').val(),
-         salary: salary = $('#annualSalary').val()
-         
-    } // end newEmployee
-    console.log('Added Employee: ', newEmployee);
-    // test to see if object calls work. it functions with user inputs created on the DOM 
+function appendAllInputs() {
+  let newEmployee = {
+    first: $("#firstName").val(),
+    last: $("#lastName").val(),
+    id: $("#idNumber").val(),
+    title: $("#employeeTitle").val(),
+    salary: $("#annualSalary").val()
+  }; // end newEmployee
+  console.log("Added Employee: ", newEmployee);
+  // test to see if object calls work. it functions with user inputs created on the DOM
 
-    // put new objects into an array 
-    newEmployeeArray.push(newEmployee);
-    // empty user inputs after submit 
-    $('#firstName').val('');
-    $('#lastName').val('');
-    $('#idNumber').val('');
-    $('#employeeTitle').val('');
-    $('#annualSalary').val('');
-    console.log('newEmployee array push', newEmployeeArray);
+  // put new objects into an array
+  newEmployeeArray.push(newEmployee);
+  // empty user inputs after submit
+  $("#firstName").val("");
+  $("#lastName").val("");
+  $("#idNumber").val("");
+  $("#employeeTitle").val("");
+  $("#annualSalary").val("");
+  console.log("newEmployee array push", newEmployeeArray);
 
-    // check if displayNewEmployee function works
-    displayNewEmployee();
+  displayNewEmployee();
 } // end appendAllInputs
 
-
 // display new employee created onto the DOM
-function displayNewEmployee  () {
-let el = $('#employeeRows');
-el.empty();
+function displayNewEmployee() {
+  let el = $("#employeeRows");
+  el.empty();
 
-    // loop through the newEmployeeArray
-    for(let employee of newEmployeeArray) {
-   
+  // loop through the newEmployeeArray
+  for (let employee of newEmployeeArray) {
     //appended to  <td> id = "employeeRow"
     el.append(` <tr>
-                <td id = "rows">${employee.first}</td>
-                <td id = "rows">${employee.last}</td>
-                <td id = "rows">${employee.id}</td>
-                <td id = "rows">${employee.title}</td>
-                <td id = "rows">${employee.salary}</td>
-                <td> <button class = "deleteButton">Delete</button> </td>
-                </tr>`);
-    } // end for of loop  
-    calculateTotalSalaries();
+                    <td id = "rows">${employee.first}</td>
+                    <td id = "rows">${employee.last}</td>
+                    <td id = "rows">${employee.id}</td>
+                    <td id = "rows">${employee.title}</td>
+                    <td id = "rows">${employee.salary}</td>
+                    <td> <button class = "deleteButton">Delete</button> </td>
+                    </tr>`);
+  } // end for of loop
+  calculateTotalSalaries();
 } // end displayNewEmployee
 
-function calculateTotalSalaries () {
-let emptyMonthlyCosts = $('#totalSalaries');
-emptyMonthlyCosts.empty();
+function calculateTotalSalaries() {
+  let emptyMonthlyCosts = $("#totalSalaries");
+  emptyMonthlyCosts.empty();
 
-let monthlyCosts = 0; 
-for(let i = 0; i < newEmployeeArray.length; i++) {
+  let monthlyCosts = 0;
+  for (let i = 0; i < newEmployeeArray.length; i++) {
     monthlyCosts += newEmployeeArray[i].salary / monthsInYear;
-}  // end for loop 
-    if (monthlyCosts > 20000){
-} // end for statment
-    console.log(monthlyCosts);
-    $('#totalSalaries').append(monthlyCosts);
+  } // end for loop
+  if (monthlyCosts > monthlyCostCap) {
+    $('#monthlyCostId').css('background', '#ff3300');
 
+  } // end for statment
+  console.log(monthlyCosts);
+  $("#totalSalaries").append(monthlyCosts);
 } // end calculateTotalSalaries
 
-function deleteRow () {
-$('.deleteButton').remove();
-} // end deleteRow 
-
-// append employee properties on submit button click 
-function appendEmployees () {
-    $('#submitButton').on('click', appendAllInputs);
-
-} // end appendEmployees
+function deleteButton() {
+  $(this)
+    .closest("tr")
+    .remove();
+} // end deleteButton
